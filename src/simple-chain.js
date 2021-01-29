ab9e0672e861993588ccc70a9884ee12635cf611
Valentina Masalkova <vrassone@gmail.com>
2021-01-29 22:00:47 +0300

    feat: implement chaimMaker function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  result: '',
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result = this.result.split('~~').length;
+    return this;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.result === '') {
+      this.result += `( ${value + ''} )`;
+      return this;
+    } else {
+      this.result += `~~( ${value + ''} )`;
+      return this;
+    }
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.result === null || this.result[position] === undefined) {
+      throw new Error('THROWN');
+    }  else {
+    this.result = this.result.split('~~');
+    this.result.splice(position - 1, 1);
+    this.result = this.result.join('~~');
+    return this;
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result = this.result.split('~~').reverse().join('~~');
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let chain = this.result;
+    this.result = '';
+    return chain;
   }
 };
+
 
 module.exports = chainMaker;
