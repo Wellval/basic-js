a03f9f1013d873e61878949d7aea583225cf9f08
Valentina Masalkova <vrassone@gmail.com>
2021-03-16 18:01:07 +0300

    fix: fix errors

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,16 +4,16 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  sampleActivity = parseInt(sampleActivity);
-  if (sampleActivity <= 0 || typeof(sampleActivity) !== Number || sampleActivity > 15) {
+  if (Number.parseInt(sampleActivity) <= 0 || typeof(sampleActivity) !== 'string' || isNaN(Number.parseInt(sampleActivity)) || Number.parseInt(sampleActivity) > 15 || !sampleActivity) {
     return false;
   }
   // throw new CustomError('Not implemented');
   else {
+  sampleActivity = parseFloat(sampleActivity);
   let n = MODERN_ACTIVITY / sampleActivity;
   let k = 0.693 / HALF_LIFE_PERIOD;
   let t = Math.log(n) / k;
-  return Math.ceil(t);
+  return (Math.ceil(t) > 0) ? Math.ceil(t) : false;
   }
   // remove line with error and write your code here
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,13 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function(str, { repeatTimes = 1, separator = "+", addition = "", additionRepeatTimes = 1, additionSeparator = ""}) {
-    let result = "";
+module.exports = function(str, { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = ''}) {
+    let result = '';
     for (let i = 0; i < repeatTimes; ++i) {
-        let add = "";
+        let add = '';
         for (let j = 0; j < additionRepeatTimes; ++j) {
-            add += addition + (j != additionRepeatTimes - 1 ? additionSeparator : "");
+            add += addition + (j != additionRepeatTimes - 1 ? additionSeparator || '|' : '');
         }
-        result += str + add + ( i != repeatTimes - 1 ? separator : "")
+        result += str + add + ( i != repeatTimes - 1 ? separator : '')
     }
     return result;
 }
